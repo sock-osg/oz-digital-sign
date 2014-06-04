315d9b24ca547801a2cba9d574650f0d43b715c5
Sam <sam@sam-ubudes.(none)>
2014-06-04 01:26:07 -0500

    Carga inicial del proyecto

---
+++ src/main/java/com/oz/digital/sign/window/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.oz.digital.sign.window;
+
+/**
+ *
+ * @author <a href="mailto:sock.sqt@gmail.com">Samuel Quintana</a>
+ * @since Jun 3, 2014, 11:58:45 PM
+ */
+public class NewClass {
+
+}
